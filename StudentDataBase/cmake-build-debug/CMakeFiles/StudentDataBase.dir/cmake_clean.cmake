f911472f02ae29cfb4a92860c5bb13dcbac8cca6
yuanjingpeng <1448616092@qq.com>
2021-06-14 18:45:20 +0800

    project.DataBase--the code of original author is a totally mess,I give up it,come again for another database engineering

---
+++ StudentDataBase/cmake-build-debug/CMakeFiles/StudentDataBase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentDataBase.dir/main.c.o"
+  "StudentDataBase.pdb"
+  "StudentDataBase"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StudentDataBase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
